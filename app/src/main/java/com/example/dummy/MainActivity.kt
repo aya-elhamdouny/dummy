0d7731ed5e9576d06bb6d317b2201299bc187cca
aya-elhamdouny <52864131+aya-elhamdouny@users.noreply.github.com>
2021-06-14 17:58:25 +0200

    two-way databinding

--- app/src/main/java/com/example/dummy/MainActivity.kt
+++ app/src/main/java/com/example/dummy/MainActivity.kt
@@ -11,10 +11,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: ViewModel
     private val movie : Movie = Movie("Beaty and the beast" ,
-        "Belle, a village girl, embarks on a journey to save her father " +
-                "from a creature that has locked him in his dungeon. Eventually," +
-                " she learns that the creature is an enchanted prince who has been cursed.",
-        "https://upload.wikimedia.org/wikipedia/en/d/d6/Beauty_and_the_Beast_2017_poster.jpg")
+            "Belle, a village girl, embarks on a journey to save her father " +
+                    "from a creature that has locked him in his dungeon. Eventually," +
+                    " she learns that the creature is an enchanted prince who has been cursed.",
+            "https://upload.wikimedia.org/wikipedia/en/d/d6/Beauty_and_the_Beast_2017_poster.jpg")
 
 
 
--- app/src/main/java/com/example/dummy/Movie.kt
+++ app/src/main/java/com/example/dummy/Movie.kt
@@ -1,11 +1,10 @@
 package com.example.dummy
 
-
-
-
+import androidx.databinding.ObservableField
+import java.util.*
 
 
 data class Movie(
     var title: String = "",
     var details: String = "",
-    var url : String = "", )
+    var url : String = "")
